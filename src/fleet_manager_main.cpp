7272ea7736804b3ead35c56a9146bae5feca8cb8
Kishore V <vkishore9701@gmail.com>
2024-03-01 23:55:38 +0530

    Created a cpp file main function for fleet manager

---
+++ src/fleet_manager_main.cpp
@@ -0,0 +1,9 @@
+/*-----A cpp file for the main function for the fleet manager------*/
+
+#include<iostream>
+
+int main()
+{
+    std::cout << "Inside the main function of fleet manager" << std::endl;
+    return 0;
+}
