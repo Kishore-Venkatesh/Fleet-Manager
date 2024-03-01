b5d37cfeadbaaa41f0f5390312a71083c82419dc
Kishore V <vkishore9701@gmail.com>
2024-03-02 00:09:00 +0530

    Created a header file for Xml data parser

---
+++ include/fleet_manager_xml_data_parser.hpp
@@ -0,0 +1,6 @@
+/*-----------A header file for class FleetManager_XmlDataParser------------*/
+
+#ifndef FLEET_MANAGER_XML_DATA_PARSER_HPP
+#define FLEET_MANAGER_XML_DATA_PARSER_HPP
+
+#endif  //FLEET_MANAGER_XML_DATA_PARSER_HPP
